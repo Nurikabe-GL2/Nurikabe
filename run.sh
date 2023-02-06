4df494c55f4840771be8133a9c96ee09001e3898
vanvanE <themix877@gmail.com>
2023-02-06 15:00:24 +0100

    correction techniques

---
+++ run.sh
@@ -0,0 +1,1 @@
+mvn javafx:run
--- src/main/java/io/github/freya022/nurikabe/controller/TechniqueController.java
+++ src/main/java/io/github/freya022/nurikabe/controller/TechniqueController.java
@@ -3,23 +3,12 @@ package io.github.freya022.nurikabe.controller;
 import io.github.freya022.nurikabe.Logging;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import org.slf4j.Logger;
 
-import io.github.freya022.nurikabe.controller.MenuPrincipalController;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.slf4j.Logger;
-
-import java.io.IOException;
-import io.github.freya022.nurikabe.Utils;
-import io.github.freya022.nurikabe.controller.TechniqueController;
-
 // extends VBox car c'est la racine du menu principal
-public class MenuPrincipalController extends VBox {
+public class TechniqueController extends VBox {
     private static final Logger LOGGER = Logging.getLogger();
 
     @FXML
@@ -29,18 +18,7 @@ public class MenuPrincipalController extends VBox {
 
     @FXML
     private void onMethodsAction(ActionEvent event) {//technique
-        try {
-                //Charge le menu principal, le contrôleur étend le type du nœud racine
-                final var controller = Utils.loadFxml(new TechniqueController(), "Techniques");
-                System.out.println("gdf");
-
-                final Scene scene = new Scene(controller); //Le conteneur de noeud racine
-                final Stage stage = new Stage(); //La fenêtre
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException e) {
-                LOGGER.error("An exception occurred while constructing the techniques frame", e);
-            }
+        LOGGER.info("Bouton technique", ((Button) event.getTarget()).getText());
     }
 
     @FXML
