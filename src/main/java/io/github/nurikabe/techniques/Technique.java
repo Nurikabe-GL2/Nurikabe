5bee65bcc94b35a7c0bc5b74021ff10ba02b5a46
vanvanE <themix877@gmail.com>
2023-03-08 11:42:36 +0100

    implémentation techniques indice séparé par case blanche

--- src/main/java/io/github/nurikabe/techniques/Technique.java
+++ src/main/java/io/github/nurikabe/techniques/Technique.java
@@ -1,5 +1,6 @@
 package main.java.io.github.nurikabe.techniques;
 
+import io.github.nurikabe.CaseNombre;
 import io.github.nurikabe.Niveau;
 /**
  * Interface des différentes techniques de jeu
@@ -7,10 +8,24 @@ import io.github.nurikabe.Niveau;
 public interface Technique {
     PositionTechniques tester(Niveau grille);
 
+    /**
+     * Méthode pour tester si une case est valide ou non (indice valide et case blanche)
+     * @param grille la grille courante
+     * @param x la coordonnée x
+     * @param y la coordonnée y
+     * @return true si elle est valide ou false
+     */
     default boolean estValide(Niveau grille,int x, int y)
     {
         if(x<0||y<0||y>=grille.get_hauteur()||x>=grille.get_largeur())
             return false;
         return(grille.get(x).get_case().equals("b"));
+    }
+
+    default boolean estValideCaseNombreEspace(Niveau grille,int x, int y)
+    {
+        if(x<0||y<0||y>=grille.get_hauteur()||x>=grille.get_largeur())
+            return false;
+        return(grille.get(x).get_case() instanceof CaseNombre);
     }
 }
