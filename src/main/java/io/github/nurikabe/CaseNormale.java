72136f57aac9ed1ef9ac1b960d85f1b8bacbadbb
vanvanE <themix877@gmail.com>
2023-03-08 09:19:08 +0100

    correctin bug

--- src/main/java/io/github/nurikabe/CaseNormale.java
+++ src/main/java/io/github/nurikabe/CaseNormale.java
@@ -82,8 +82,8 @@ public class CaseNormale extends Case{
         
         }
 
-        if(!appel)
-          undoStack.push(jouer);
+        /*if(!appel)
+          undoStack.push(jouer);*/
 
 
     }
