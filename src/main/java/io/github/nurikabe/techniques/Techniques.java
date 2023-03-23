00633f5acfe08ff7c55549a9c07d74eed37be6d3
freya02 <41875020+freya022@users.noreply.github.com>
2023-03-23 17:55:15 +0100

    Arrangement des priorités des techniques

--- src/main/java/io/github/nurikabe/techniques/Techniques.java
+++ src/main/java/io/github/nurikabe/techniques/Techniques.java
@@ -6,16 +6,18 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class Techniques {
+    //L'ordre des techniques est important pour savoir laquelle est prioritaire.
+    // Les techniques prioritaires sont les moins compliquées
     public static final List<Technique> TECHNIQUES = List.of(
-            new CarreIsole(),
-            new ContinuiteDeLile(),
-            new ContinuiteDunMur(),
-            new EviterZoneMur(),
-            new IleDeUn(),
-            new IleExtensibleDansDeuxDirections(),
-            new IleExtensionDepuisUnIndice(),
-            new IndiceAdjacentsEnDiagonale(),
-            new IndicesSeparerParCaseBlanche()
+            new IleDeUn(),                              //Demarrage 1
+            new IndicesSeparerParCaseBlanche(),         //Demarrage 2
+            new IndiceAdjacentsEnDiagonale(),           //Demarrage 3
+            new CarreIsole(),                           //Basique 1
+            new ContinuiteDunMur(),                     //Basique 2
+            new IleExtensionDepuisUnIndice(),           //Basique 4
+            new IleExtensibleDansDeuxDirections(),      //Basique 5
+            new ContinuiteDeLile(),                     //Basique 7
+            new EviterZoneMur()                         //Basique 9
     );
 
     @Nullable
