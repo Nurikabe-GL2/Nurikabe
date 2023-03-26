5215a589c63eb95ee4f745f2998af400db725328
freya02 <41875020+freya022@users.noreply.github.com>
2023-03-26 16:53:26 +0200

    Affichage de toutes les techniques sur le plateau

--- src/main/java/io/github/nurikabe/controller/NiveauController.java
+++ src/main/java/io/github/nurikabe/controller/NiveauController.java
@@ -3,6 +3,7 @@ package io.github.nurikabe.controller;
 import io.github.nurikabe.Niveau;
 import io.github.nurikabe.Utils;
 import io.github.nurikabe.techniques.PositionTechniques;
+import io.github.nurikabe.techniques.Technique;
 import io.github.nurikabe.techniques.Techniques;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +15,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Class public représentant le controller des techniques héritant de la classe VBox, la racine du menu principal
@@ -47,6 +51,8 @@ public class NiveauController extends VBox {
     @FXML private Button buttonAide;
 
     @FXML private TabPane tabPane;
+
+    @FXML private VBox techniquesBox;
 
     /**
      * Le constructeur de la classe TechniquesController
@@ -91,6 +97,21 @@ public class NiveauController extends VBox {
          }
 
          stage.setScene(new Scene(this));
+    }
+
+    @FXML
+    private void initialize() throws IOException {
+        final Map<String, List<Technique>> categoriesTechniques = new LinkedHashMap<>();
+        for (Technique technique : Techniques.TECHNIQUES) {
+            categoriesTechniques.computeIfAbsent(technique.getCategorie(), s -> new ArrayList<>()).add(technique);
+        }
+
+        for (var entry : categoriesTechniques.entrySet()) {
+            final var nomCategorie = entry.getKey();
+            final var techniques = entry.getValue();
+            final var controller = Utils.loadFxml(new CategorieTechniqueController(nomCategorie, techniques), "_CategorieTechnique");
+            techniquesBox.getChildren().add(controller);
+        }
     }
 
     /**
