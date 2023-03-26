4a759d6f1f53e31425c7e33592953b000c59abb9
freya02 <41875020+freya022@users.noreply.github.com>
2023-03-26 16:30:57 +0200

    Simplification CarreIsole

--- src/main/java/io/github/nurikabe/techniques/basique/CarreIsole.java
+++ src/main/java/io/github/nurikabe/techniques/basique/CarreIsole.java
@@ -20,41 +20,35 @@ public class CarreIsole extends Technique {
 
     /**
      * Méthode de parcours de la grille, elle teste toute les cases et vérifie que la technique est applicable, si oui elle ajoute la case sur laquelle appliqué la technique à la liste
+     *
      * @param grille la grille en question
+     *
      * @return une Position technique ou null
      */
     @Override
-    public PositionTechniques tester(Niveau grille)
-    {
-        
-        for(int i=0;i<grille.recupLargeur();i++)
-        {
-            for(int j=0;j<grille.get_hauteur();j++)
-            {
-                if(grille.recupCase(i, j) instanceof CaseNormale && grille.recupCase(i, j).recupContenuCase().equals("b"))
-                {
+    public PositionTechniques tester(Niveau grille) {
+        for (int i = 0; i < grille.recupLargeur(); i++) {
+            for (int j = 0; j < grille.get_hauteur(); j++) {
+                if (grille.recupCase(i, j) instanceof CaseNormale && grille.recupCase(i, j).recupContenuCase().equals("b")) {
                     List<Cible> liste = new ArrayList<>();
 
-		//test si la case de gauche de la case courante est valide ou que c'est une case noir
-                    if(estCoordonneeValide(grille,i-1,j) && grille.recupCase(i - 1, j).recupContenuCase().equals("n"))
-                    {
-                    	//test si la case de droite de la case courante est valide ou que c'est une case noir
-                    	if(estCoordonneeValide(grille,i+1,j) && grille.recupCase(i + 1, j).recupContenuCase().equals("n"))
-                    	{
-                    		//test si la case en bas de la case courante est valide ou que c'est une case noir
-                    		if(estCoordonneeValide(grille,i,j-1) && grille.recupCase(i, j - 1).recupContenuCase().equals("n"))
-                    		{
-                    			//test si la case en bas de la case courante est valide ou que c'est une case noir
-                    			if(estCoordonneeValide(grille,i,j+1) && grille.recupCase(i, j + 1).recupContenuCase().equals("n"))
-                    				liste.add(new Cible(i,j, "n"));
-                    		}
-                    		
-					if(!liste.isEmpty())
-                        			return new PositionTechniques(this, liste);
-                    	}
+                    //test si la case de gauche de la case courante est valide ou que c'est une case noir
+                    if (estCaseNoire(grille, i - 1, j)) {
+                        //test si la case de droite de la case courante est valide ou que c'est une case noir
+                        if (estCaseNoire(grille, i + 1, j)) {
+                            //test si la case en bas de la case courante est valide ou que c'est une case noir
+                            if (estCaseNoire(grille, i, j - 1)) {
+                                //test si la case en bas de la case courante est valide ou que c'est une case noir
+                                if (estCaseNoire(grille, i, j + 1))
+                                    liste.add(new Cible(i, j, "n"));
+                            }
+
+                            if (!liste.isEmpty())
+                                return new PositionTechniques(this, liste);
+                        }
                     }
                 }
-            }   
+            }
         }
         return null;
     }
