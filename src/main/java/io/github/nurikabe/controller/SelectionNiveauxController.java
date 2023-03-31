03691a925e8a1b9da0aef9db58ea253539b76f97
freya022 <41875020+freya022@users.noreply.github.com>
2023-03-31 15:55:33 +0200

    Correction mode aventure restant bloqué

--- src/main/java/io/github/nurikabe/controller/SelectionNiveauxController.java
+++ src/main/java/io/github/nurikabe/controller/SelectionNiveauxController.java
@@ -167,7 +167,7 @@ public class SelectionNiveauxController extends VBox {
             if (metadonneesSauvegarde.estComplete()) {
                 button.getStyleClass().add("complete");
                 button.setText("Complété");
-            } else if (i == 0 || metadonneesSauvegardes.get(sauvegardesSize - 1).estComplete()) {
+            } else if (i == 0 || metadonneesSauvegardes.get(i - 1).estComplete()) {
                 //Le niveau est déverrouillé si c'est le premier, ou alors que le précédent est complet
                 button.setText(metadonneesSauvegarde.existe() ? "Continuer" : "Jouer");
                 button.setOnMouseClicked(event -> {
